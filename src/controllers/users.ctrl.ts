421552b1dd3e1a2c1afb2c9f0ab9bc91dc04cd17
philippe-jonathan <39793063+philippe-jonathan@users.noreply.github.com>
2023-03-24 10:45:01 +0100

    update project

--- src/controllers/users.ctrl.ts
+++ src/controllers/users.ctrl.ts
@@ -1,8 +1,9 @@
 import { Next, ParameterizedContext } from "koa"
 import { InternalError, InvalidPayload, ResourceNotFoundError } from "../helpers/ControllerErrors"
+import PetRepository from "../repositories/pets/interface"
 import UserRepository from "../repositories/users/interface"
 
-export default function getController(userRepo: UserRepository) {
+export default function getController(userRepo: UserRepository, petRepo: PetRepository) {
 
   async function addUser(ctx: ParameterizedContext, next: Next) {
     // Validate payload
@@ -23,18 +24,18 @@ export default function getController(userRepo: UserRepository) {
   }
 
   async function getUser(ctx: ParameterizedContext, next: Next) {
-    try {
-      const user = await userRepo.get(ctx.params.id)
+    const user = await userRepo.get(ctx.params.id)
 
-      if (!user) {
-        throw new ResourceNotFoundError("User not found")
-      }
-
-      ctx.status = 200
-      ctx.body = user.toJSON()
-    } catch (e: any) {
-      throw new InternalError(e.message)
+    if (!user) {
+      throw new ResourceNotFoundError("User not found")
     }
+
+    const pets = await petRepo.list(user.pets)
+
+    const displayableUser = { ...user.toJSON(), pets: pets }
+
+    ctx.status = 200
+    ctx.body = displayableUser
     next()
   }
 
@@ -43,6 +44,12 @@ export default function getController(userRepo: UserRepository) {
     ctx.body = { message: "Not yet implemented" }
 
     next()
+  }
+
+  async function addPet(ctx: ParameterizedContext, next: Next) {
+    // users/:id/pets
+    // POST
+    // body: PetId[]
   }
 
   async function deleteUser(ctx: ParameterizedContext, next: Next) {
